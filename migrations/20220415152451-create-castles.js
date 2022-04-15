e5b961d3fd63dce6b46484d589be4f0266c57b8e
Eloy Sauceda <eloysauceda4@outlook.com>
2022-04-15 10:48:22 -0500

    adding models

---
+++ migrations/20220415152451-create-castles.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('castles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      forTypes: {
+        type: Sequelize.STRING
+      },
+      servantCount: {
+        type: Sequelize.INTEGER
+      },
+      incomePerCastle: {
+        type: Sequelize.INTEGER
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('castles');
+  }
+};
