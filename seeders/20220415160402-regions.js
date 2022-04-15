ac652471346cb0285b10600d74dd4b25d6b1a8e8
Eloy Sauceda <eloysauceda4@outlook.com>
2022-04-15 13:32:00 -0500

    add seed data

---
+++ seeders/20220415160402-regions.js
@@ -0,0 +1,46 @@
+'use strict';
+const { User, sequelize} = require('../models')
+const user = require('../models/user')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    let users = await User.findOne({ order: sequelize.random(), raw: true})
+  
+    return queryInterface.bulkInsert('regions', 
+   [{
+    Type: 'Empire',
+    name: 'First Bulgarian Empire',
+    population: 350,
+    numberOfNobles: 1,
+    capitalCity: 'Pliska',
+   createdAt: new Date(),
+   updatedAt: new Date()
+    }, 
+   
+    
+    {
+      Type: 'Kingdom',
+      name: 'England',
+      population: 879,
+      numberOfNobles: 1,
+      capitalCity: 'Wnchester, London',
+     createdAt: new Date(),
+     updatedAt: new Date()
+      }, 
+      {
+        Type: 'Duchy',
+        name: 'Athens',
+        population: 700,
+        numberOfNobles: 1,
+        capitalCity: 'Athens',
+       createdAt: new Date(),
+       updatedAt: new Date() 
+      }]
+    );
+   
+ },
+
+ down: async (queryInterface, Sequelize) => {
+   return queryInterface.bulkDelete('regions')
+ }
+};
