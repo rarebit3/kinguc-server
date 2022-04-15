f9b5ebe17af943f0141572bf4cfff741c9e74213
David Wu <dwu0508@gmail.com>
2022-04-15 13:35:44 -0500

    stubbed CastleController

--- controllers/RegionController.js
+++ controllers/RegionController.js
@@ -1,9 +1,8 @@
-const res = require('express/lib/response')
-const { Region } = require('../models')
+const { Regions } = require('../models')
 
 const GetAllRegions = async (req, res) => {
     try {
-        const regions = await Region.findAll()
+        const regions = await Regions.findAll()
         res.send(regions)
     } catch (error) {
         throw error
@@ -12,7 +11,7 @@ const GetAllRegions = async (req, res) => {
 
 const GetRegionDetails = async (req, res) => {
     try {
-        const region = await Region.findByPk(
+        const region = await Regions.findByPk(
             req.params.property_id
         )
         res.send(region)
@@ -23,7 +22,7 @@ const GetRegionDetails = async (req, res) => {
 
 const CreateRegion = async (req, res) => {
     try {
-        const newRegion = await Region.create(req.body)
+        const newRegion = await Regions.create(req.body)
         res.send(newRegion)
     } catch (error) {
         throw error
@@ -33,9 +32,10 @@ const CreateRegion = async (req, res) => {
 const UpdateRegion = async (req, res) => {
     try {
         const regionId = parseInt(req.params.region_id)
-        updatedRegion = await Region.update(req.body, {
+        const updatedRegion = await Regions.update(req.body, {
             where: { id: regionId}
         })
+        res.send(updatedRegion)
     } catch (error) {
             throw error
     }
@@ -44,7 +44,7 @@ const UpdateRegion = async (req, res) => {
 const DeleteRegion = async (req, res) => {
     try {
         const regionId = parseInt(req.params.region_id)
-        await Region.destroy({
+        await Regions.destroy({
             where: { id: regionId }
         })
         res.send({ message: `Deleted region with an id of ${regionId}`})
