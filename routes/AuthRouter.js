e848e8a29e9ec9b97e961fb7400898b228498eff
David Wu <dwu0508@gmail.com>
2022-04-19 10:47:54 -0500

    added controller/route for checkseesion

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -5,5 +5,10 @@ const middleware = require('../middleware')
 
 router.post('/login', controller.Login)
 router.post('/register', controller.Register)
-
+router.get(
+    '/session',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.CheckSession
+  )
 module.exports = router
