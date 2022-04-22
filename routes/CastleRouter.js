01d24c04dcc47e1f9671670473a2144ab5326460
David Wu <dwu0508@gmail.com>
2022-04-22 09:32:56 -0500

    added delete castle

--- routes/CastleRouter.js
+++ routes/CastleRouter.js
@@ -1,11 +1,21 @@
 const Router = require('express').Router()
 const controller = require('../controllers/CastleController')
+const middleware = require('../middleware')
 
 Router.get('/search', controller.SearchCastles)
+
 Router.get('/', controller.GetAllCastles)
+
 Router.get('/:castle_id', controller.GetCastleDetails)
+
 Router.post('/', controller.CreateCastle)
-Router.put('/:castle_id', controller.UpdateCastle)
+
+Router.put('/:castle_id',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.UpdateCastle
+)
+
 Router.delete('/:castle_id', controller.DeleteCastle)
 
 module.exports = Router
